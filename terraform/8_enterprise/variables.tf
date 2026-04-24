7b0e70fe85ddfa89d6688c1f1931f58f450d28da
Joshua Balogun <joshua.balogun01@gmail.com>
2026-04-24 12:42:04 +0100

    Added clean ups

--- terraform/8_enterprise/variables.tf
+++ terraform/8_enterprise/variables.tf
@@ -22,3 +22,15 @@ variable "alarm_email" {
   type    = string
   default = ""
 }
+
+variable "bedrock_region" {
+  description = "AWS region for Bedrock (may differ from main region)"
+  type        = string
+  default     = "eu-west-1"
+}
+
+variable "bedrock_model_id" {
+  description = "Bedrock model ID to monitor (e.g., amazon.nova-pro-v1:0)"
+  type        = string
+  default     = "openai.gpt-oss-120b-1:0"
+}
